9e2020d9fd4119f974a13fd20c8a9188945953a4
Medliarskyi <med.uras111@gmail.com>
2024-10-15 14:19:35 +0300

    first master commit

---
+++ UnitTest61it/UnitTest61it.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab61it/lab61it.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest61it
+{
+	TEST_CLASS(UnitTest61it)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int arr1[] = { -2, -4, -6, 1, 2, 3 };
+			Assert::AreEqual(sum(arr1, 6), -12);
+		}
+	};
+}
